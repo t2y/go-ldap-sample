34042f7cc0045987f876a12cf04dfd3b23846115
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2023-06-01 09:18:26 +0900

    add search example

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/t2y/go-ldap-sample
+
+go 1.20
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/go-ldap/ldap v3.0.3+incompatible
+)
+
+require (
+	github.com/stretchr/testify v1.7.2 // indirect
+	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+
+	"github.com/caarlos0/env"
+	"github.com/go-ldap/ldap"
+)
+
+type Config struct {
+	URL       string `env:"LDAP_URL,required"`
+	User      string `env:"LDAP_USER,required"`
+	Password  string `env:"LDAP_PASSWORD,required"`
+	DN_SUFFIX string `env:"DN_SUFFIX,required"`
+}
+
+func connect(c *Config) (*ldap.Conn, error) {
+	conn, err := ldap.DialTLS(
+		"tcp",
+		c.URL,
+		&tls.Config{
+			InsecureSkipVerify: true,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	if err := conn.Bind(c.User, c.Password); err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func search(conn *ldap.Conn, req *ldap.SearchRequest) error {
+	r, err := conn.Search(req)
+	if err != nil {
+		return err
+	}
+	for _, entry := range r.Entries {
+		fmt.Printf("%s: %v\n", entry.DN, entry.GetAttributeValue("cn"))
+	}
+	log.Printf("entries length: %d", len(r.Entries))
+	return nil
+}
+
+const (
+	sizeLimit = 0
+	timeLimit = 0
+)
+
+func main() {
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(fmt.Errorf("failed to set: %w", err))
+	}
+	conn, err := connect(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	req := ldap.NewSearchRequest(
+		cfg.DN_SUFFIX,
+		ldap.ScopeWholeSubtree,
+		ldap.NeverDerefAliases,
+		sizeLimit,
+		timeLimit,
+		false,
+		"(objectclass=*)",
+		[]string{},
+		nil,
+	)
+	if err := search(conn, req); err != nil {
+		log.Fatal(err)
+	}
+}
