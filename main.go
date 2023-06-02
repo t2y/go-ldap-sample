948e318d9538ecd00ea5a71e72a9bfc241bb2ac5
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2023-06-02 10:44:50 +0900

    test SearchWithChannel api

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"log"
@@ -30,7 +31,7 @@ func connect(c *Config) (*ldap.Conn, error) {
 	if err := conn.Bind(c.User, c.Password); err != nil {
 		return nil, err
 	}
-	conn.Debug = true
+	//conn.Debug = true
 	return conn, nil
 }
 
@@ -43,6 +44,23 @@ func search(conn *ldap.Conn, req *ldap.SearchRequest) error {
 		fmt.Printf("%s: %v\n", entry.DN, entry.GetAttributeValue("cn"))
 	}
 	log.Printf("entries length: %d", len(r.Entries))
+	return nil
+}
+
+func search2(conn *ldap.Conn, req *ldap.SearchRequest) error {
+	i := 0
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := conn.SearchWithChannel(ctx, req)
+	for r := range ch {
+		if r.Error != nil {
+			return r.Error
+		}
+		fmt.Printf("%d: %s, %v\n", i, r.Entry.DN, r.Entry.GetAttributeValue("cn"))
+		i++
+		if i == 20010 {
+			cancel()
+		}
+	}
 	return nil
 }
 
@@ -73,7 +91,7 @@ func main() {
 		[]string{},
 		nil,
 	)
-	if err := search(conn, req); err != nil {
+	if err := search2(conn, req); err != nil {
 		log.Fatal(err)
 	}
 }
