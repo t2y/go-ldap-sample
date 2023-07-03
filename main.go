a259c52a5d87426d6ecb672cbb7bbbf37f90b201
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2023-07-03 10:10:32 +0900

    update search function

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func search(conn *ldap.Conn, req *ldap.SearchRequest) error {
 	return nil
 }
 
-func searchAsync(conn *ldap.Conn, req *ldap.SearchRequest) error {
+func searchAsyncWithCancel(conn *ldap.Conn, req *ldap.SearchRequest) error {
 	i := 0
 	cancelNum := 200
 	ctx, cancel := context.WithCancel(context.Background())
@@ -62,6 +62,21 @@ func searchAsync(conn *ldap.Conn, req *ldap.SearchRequest) error {
 			cancel()
 			time.Sleep(1 * time.Second)
 		}
+	}
+	if err := r.Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func searchAsync(conn *ldap.Conn, req *ldap.SearchRequest) error {
+	i := 0
+	ctx := context.Background()
+	r := conn.SearchAsync(ctx, req, 0)
+	for r.Next() {
+		entry := r.Entry()
+		fmt.Printf("%d: %s, %v\n", i, entry.DN, entry.GetAttributeValue("cn"))
+		i++
 	}
 	if err := r.Err(); err != nil {
 		return err
